05820b0d4cd51ca8922a21823aad181cc9a2cd3e
Jediman <sike.david@gmail.com>
2026-02-25 19:29:30 +0100

    +0.5 ora (osszesen: 3.5 ora)

--- DrinkCLI/DrinkCLI/Program.cs
+++ DrinkCLI/DrinkCLI/Program.cs
@@ -26,13 +26,41 @@ namespace DrinkCLI
             sr.Close();
             fs.Close();
 
-            Console.WriteLine("5. feladat");
-            Console.WriteLine("");
-            for (int i = 0; i < 5; i++)
+            void taskNumberFive()
             {
-                civilian[i].citizens();
+                Console.WriteLine("5. feladat");
+                Console.WriteLine("");
+                for (int i = 0; i < 5; i++)
+                {
+                    civilian[i].citizens();
+                }
             }
 
+            void taskNUmberSix()
+            {
+
+            }
+
+            void taskNumberSeven(string filename)
+            {
+                FileStream fr = new FileStream(filename, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fr);
+                for (int i = 0; i < civilian.Count; i++)
+                {
+                    if (civilian[i].Age1 >= 18 && civilian[i].Age1 <= 65 && civilian[i].Group.PopulationGroupName1 == "Asian" && civilian[i].Drinking1 == "Yes")
+                    {
+                        sw.WriteLine($"{civilian[i].Age1};{civilian[i].State1};{civilian[i].SchoolDropout1}");
+                        //Console.WriteLine($"{civilian[i].Age1};{civilian[i].State1};{civilian[i].SchoolDropout1}");
+                    }
+                }
+                sw.Close();
+                fr.Close();
+                Console.WriteLine($"File save success: {filename}");
+            }
+
+            taskNumberFive();
+
+            taskNumberSeven("asian.txt");
             // Console.WriteLine($"sikeres adatbeolvasas: {civilian.Count}");
             Console.ReadKey(); // Hogy ne zarja be a console ablakot!
         }
